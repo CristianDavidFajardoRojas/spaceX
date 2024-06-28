663f4aef3e2f649da2bb2d3dc32c0503a5db1255
CristianDavidFajardoRojas <cfajardorojas98@gmail.com>
2024-06-28 08:34:54 -0500

    feat: :sparkles: Footer buttons.
    Funcionalidad de los botones de los botones del footer.

--- js/main.js
+++ js/main.js
@@ -2,55 +2,105 @@ import { getAllRocketsData, getRocketData } from "./modules/rocketsData.js";
 import { rocketHTML } from "./components/rockets.js";
 
 
+const allPaginacion = async(funcionAllData, primerDato, dictAMostrar, plantilla) => {
+let allData = funcionAllData;
 
-let rockets = (await getAllRocketsData());
+////Muestra el primer dict.
+let body = document.querySelector(".body")
+body.innerHTML = primerDato;
 
-////////
-
+//Paginacion.
 let currentPage = 0;
 let itemsPerPage = 4;
 
-let body = document.querySelector(".body")
-body.innerHTML = rocketHTML(await getRocketData(1));
-
-
-function changePage(page) {
+const changePage = page => {
     currentPage = page;
     initPagination();
 }
 
-const initPagination = () => {
+const initPagination = async() => {
     const pagination = document.getElementById('pagination');
     pagination.innerHTML = '';
 
     const start = currentPage * itemsPerPage;
-    const end = Math.min(start + itemsPerPage, rockets.length);
+    const end = Math.min(start + itemsPerPage, allData.length);
 
     for (let i = start; i < end; i++) {
         const pageButton = document.createElement('button');
         pageButton.innerText = i + 1;
-        pageButton.onclick = async() => {body.innerHTML = rocketHTML(await getRocketData(i + 1))
-        initPagination()};
+        pageButton.onclick = async() => {body.innerHTML = plantilla(await dictAMostrar(i + 1));
+        initPagination();
+        footer();};
         pagination.appendChild(pageButton);
     }
 
-    const totalPages = Math.ceil(rockets.length / itemsPerPage);
+    const totalPages = Math.ceil(allData.length / itemsPerPage);
 
     if (currentPage > 0) {
         const prevButton = document.createElement('button');
-        prevButton.innerText = 'Prev';
+        prevButton.innerText = '<<';
         prevButton.onclick = () => changePage(currentPage - 1);
-        pagination.appendChild(prevButton);
+        pagination.insertBefore(prevButton, pagination.firstChild);
+    } else if (currentPage == 0 && allData.length > itemsPerPage){
+        const prevButton = document.createElement('button');
+        prevButton.innerText = '<<';
+        prevButton.onclick = () => changePage(currentPage = totalPages - 1);
+        pagination.insertBefore(prevButton, pagination.firstChild);
     }
 
     if (currentPage < totalPages - 1) {
         const nextButton = document.createElement('button');
-        nextButton.innerText = 'Next';
+        nextButton.innerText = '>>';
         nextButton.onclick = () => changePage(currentPage + 1);
         pagination.appendChild(nextButton);
+    } else if(currentPage = totalPages - 1) {
+        const nextButton = document.createElement('button');
+        nextButton.innerText = '>>';
+        nextButton.onclick = () => changePage(0);
+        pagination.appendChild(nextButton);
     }
+
 }
 
+//Inicia la paginaciòn.
 initPagination();
+}
 
+// Principio de la pagina, recien entra.
+allPaginacion(await getAllRocketsData(), rocketHTML(await getRocketData(1)), getRocketData, rocketHTML);
+
+
+const footer = async() => {
+    let articlesFooter = document.querySelectorAll(".footer > section > article");
+
+    articlesFooter.forEach(article => {
+        article.addEventListener('click', async e => {
+            let name = article.querySelector("p");
+            console.log(name.textContent);
+
+            if(name.textContent == "Rockets"){
+                allPaginacion(await getAllRocketsData(), rocketHTML(await getRocketData(1)), getRocketData, rocketHTML);
+                footer();
+            } 
+            
+            if(name.textContent == "Capsules"){
+                console.log("asdsad")
+                allPaginacion(await getAllRocketsData(), rocketHTML(await getRocketData(3)), getRocketData, rocketHTML);
+                footer();
+            }
+
+
+
+
+
+
+
+
+
+
+        })
+    })
+}
+
+footer();
 
